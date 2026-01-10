8d8b028c327380cb699c3dcf207292784d09c694
DESKTOP-QER4CFP\Damian-PC <damianjolon@gmail.com>
2026-01-10 16:51:10 -0600

    Fix Vercel API catch-all route

--- user-app/api/index.js
+++ user-app/api/index.js
@@ -1,3 +1,6 @@
-// user-app/api/[...path].js
-const app = require('../backend/src/index.js');
-module.exports = app;
+const app = require("../backend/src/index.js");
+
+module.exports = (req, res) => {
+  req.url = req.url.replace(/^\/api(?=\/|$)/, "") || "/";
+  return app(req, res);
+};
